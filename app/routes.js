8211baa74b3571fd29d2103322e7cecdfada7bce
ganesh4991 <ganesh4991@gmail.com>
2017-07-31 22:20:09 -0400

    initial commit

---
+++ app/routes.js
@@ -0,0 +1,17 @@
+module.exports = function(app) {
+
+    // server routes ===========================================================
+    // handle things like api calls
+    // authentication routes
+
+
+    // route to handle creating goes here (app.post)
+    // route to handle delete goes here (app.delete)
+
+    // frontend routes =========================================================
+    // route to handle all angular requests
+    app.get('*', function(req, res) {
+        res.sendfile('./public/views/index.html'); // load our public/index.html file
+    });
+
+};
